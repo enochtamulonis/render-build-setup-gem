499d10b085e17ad34c183605f4dabcd22939e899
enoch tamulonis <yungboiiindigo@gmail.com>
2024-01-25 17:57:05 -0600

    refactor

--- lib/generators/render_build_setup/install_generator.rb
+++ lib/generators/render_build_setup/install_generator.rb
@@ -1,14 +1,10 @@
 require 'rails/generators/base'
 
-module RenderBuildSetup
-  module Generators
-    class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('templates', __dir__)
-
-      def create_script_file
-        template 'render-build.sh', 'bin/render-build.sh'
-        chmod 'bin/render-build.sh', 0755
-      end
-    end
+class RenderBuildSetup < Rails::Generators::Base
+  source_root File.expand_path('templates', __dir__)
+  desc "This generator creates an initializer file at config/initializers"
+  def create_script_file
+    template 'render-build.sh', 'bin/render-build.sh'
+    chmod 'bin/render-build.sh', 0755
   end
 end
