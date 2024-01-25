f1a71637677c8102223c74cfe52c9d4f832c7be2
enoch tamulonis <yungboiiindigo@gmail.com>
2024-01-25 17:59:04 -0600

    rename file

--- lib/generators/render_build_setup/render_build_setup.rb
+++ lib/generators/render_build_setup/render_build_setup.rb
@@ -3,7 +3,8 @@ require 'rails/generators/base'
 class RenderBuildSetup < Rails::Generators::Base
   source_root File.expand_path('templates', __dir__)
   desc "This generator creates an initializer file at config/initializers"
-  def create_script_file
+
+  def install
     template 'render-build.sh', 'bin/render-build.sh'
     chmod 'bin/render-build.sh', 0755
   end
